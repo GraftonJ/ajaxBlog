a45393178d71be27576b79b66d1769c904a966ff
Josh <grafton.josh@gmail.com>
2018-10-19 14:24:11 -0600

    First commit on new repo. Setup migrations.

---
+++ migrations/20181019140813_blog_data.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('blog_data', function(table) {
+    table.increments('id')
+    table.string('title', 255).notNullable().defaultTo('')
+    table.text('content').notNullable().defaultTo('')
+    table.timestamps(true, true)
+  })
+
+}
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('blog_data')
+};
