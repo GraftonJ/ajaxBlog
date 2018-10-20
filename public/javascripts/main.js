9903e1991d17c2af464724d8b82278e05adef8e9
Josh <grafton.josh@gmail.com>
2018-10-20 16:17:29 -0600

    Added Create Post area on page with click

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Connected to Main.js');
   getPosts()
+  createPost()
 })
 
 function getPosts() {
@@ -12,17 +13,17 @@ function getPosts() {
         //Create links of blog post titles in the left sidebar
         let sideTitle = document.createElement('a')
         sideTitle.innerHTML = `
-          <a href="#!" class="collection-item sidebar-title" data-id="${post.id}">${post.title}</a>
+          <a href="#view" class="collection-item sidebar-title" data-id="${post.id}">${post.title}</a>
           `
         //Insert the blog post titles in the sidebar through DOM
         sidebar.appendChild(sideTitle)
 
       //Show blog post clicked in left sidebar
       sideTitle.addEventListener('click', (ev) => {
-        removePost()
+        let contentArea = document.getElementById('content-area')
+        contentArea.innerHTML = ''
         let postID = ev.target.getAttribute('data-id')
         console.log(postID);
-        let contentArea = document.getElementById('content-area')
         let content = document.createElement('div')
         content.setAttribute('id', 'post-content')
         content.innerHTML = `
@@ -35,6 +36,26 @@ function getPosts() {
   })
 }
 //End of get Posts Function above
+
+//Create Post
+function createPost() {
+  const createLink = document.getElementById('create-post')
+  createLink.addEventListener('click', (ev) => {
+    let contentArea = document.getElementById('content-area')
+    contentArea.innerHTML = ''
+    let createContent = document.createElement('div')
+    createContent.setAttribute('id', 'create-content')
+    createContent.innerHTML = `
+      <h2>Create a New Post</h2>
+      <input placeholder="New Post Title Goes Here" id="title" type="text" class="validate">
+      <label for="title">Title</label>
+      <textarea placeholder="Write your content here. The text area will expand to fit your content."id="textarea1" class="materialize-textarea"></textarea>
+       <label for="textarea1">Content</label>
+    `
+    contentArea.appendChild(createContent)
+  })
+}
+
 
 const removePost = () => {
     let contentArea = document.getElementById('content-area')
